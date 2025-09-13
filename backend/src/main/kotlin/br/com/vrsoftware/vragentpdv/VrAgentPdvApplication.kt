3114928fbb5a6aa50821656ea14a525b63a03d19
eagorafelipe <eagorafelipe+github@gmail.com>
2025-09-13 15:37:50 -0300

    VAPP-001
    - Criação da estrutura do backend

---
+++ backend/src/main/kotlin/br/com/vrsoftware/vragentpdv/VrAgentPdvApplication.kt
@@ -0,0 +1,11 @@
+package br.com.vrsoftware.vragentpdv
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VrAgentPdvApplication
+
+fun main(args: Array<String>) {
+	runApplication<VrAgentPdvApplication>(*args)
+}
